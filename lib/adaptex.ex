16aa84308a69775ace635b9965d25dacb138acd6
Mike Foucault <foucault.m.a@gmail.com>
2020-06-04 09:57:09 +0000

    init macro

--- lib/adaptex.ex
+++ lib/adaptex.ex
@@ -1,18 +1,89 @@
 defmodule Adaptex do
-  @moduledoc """
-  Documentation for `Adaptex`.
-  """
+  defmodule Utils do
+    def gateway_functions(gateway), do: gateway.__protocol__(:functions)
 
-  @doc """
-  Hello world.
+    def extract_struct(adapter), do: adapter.__struct__()
 
-  ## Examples
+    def adapter_struct(adapter),
+      do: adapter |> adapter_for() |> extract_struct()
 
-      iex> Adaptex.hello()
-      :world
+    def adapter_for(adapter),
+      do: [adapter, Adaptex] |> Module.concat()
 
-  """
-  def hello do
-    :world
+    def adapter_scope(options),
+      do: Keyword.get(options, :adapter_scope, nil)
+
+    def config(options) do
+      %{
+        gateway: Keyword.get(options, :gateway, nil),
+        default_adapter: Keyword.get(options, :default_adapter, nil)
+      }
+    end
+  end
+
+  defmodule Gateway do
+    defmacro __using__(options) do
+      %{gateway: gateway, default_adapter: default_adapter} = Utils.config(options)
+
+      attribute_definition_quote =
+        quote bind_quoted: [gateway: gateway, default_adapter: default_adapter] do
+          Module.put_attribute(__MODULE__, :adaptex_gateway_api, gateway)
+
+          Module.put_attribute(
+            __MODULE__,
+            :adaptex_default_adapter,
+            Utils.adapter_for(default_adapter)
+          )
+        end
+
+      gateway_function_delegation_quote =
+        quote unquote: false do
+          varify = fn pos ->
+            Macro.var(String.to_atom("arg" <> Integer.to_string(pos)), __MODULE__)
+          end
+
+          funs =
+            @adaptex_gateway_api
+            |> Utils.gateway_functions()
+            |> Macro.escape()
+            |> List.wrap()
+
+          for {name, arity} <- funs do
+            args = :lists.map(varify, :lists.seq(2, arity))
+            call_args = [quote(do: Utils.extract_struct(@adaptex_default_adapter)) | args]
+
+            def unquote(name)(unquote_splicing(args)) do
+              unquote(@adaptex_gateway_api).unquote(name)(unquote_splicing(call_args))
+            end
+          end
+        end
+
+      quote do
+        unquote(attribute_definition_quote)
+        unquote(gateway_function_delegation_quote)
+      end
+    end
+  end
+
+  defmodule SecondaryAdapter do
+    defmacro __using__(options) do
+      %{gateway: gateway} = Utils.config(options)
+
+      adaptex_quote =
+        quote bind_quoted: [gateway: gateway, adapter_scope: Utils.adapter_scope(options)] do
+          defmodule Adaptex do
+            defstruct gateway_api: gateway,
+                      adapter_scope: adapter_scope,
+                      adapter_type: "secondary"
+          end
+
+          Module.put_attribute(__MODULE__, :adaptex_gateway_api, gateway)
+          Module.put_attribute(__MODULE__, :adaptex_current_adapter, Adaptex)
+        end
+
+      quote do
+        unquote(adaptex_quote)
+      end
+    end
   end
 end
--- test/adaptex_test.exs
+++ test/adaptex_test.exs
@@ -1,8 +1,64 @@
 defmodule AdaptexTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   doctest Adaptex
 
-  test "greets the world" do
-    assert Adaptex.hello() == :world
+  defprotocol MyInterface do
+    def foo_bar0(adapter)
+    def foo_bar1(adapter, p1, p2)
+    def foo_bar2(adapter)
+  end
+
+  defmodule MyFakeSecondaryAdapter do
+    use Adaptex.SecondaryAdapter,
+      adapter_scope: :kraken,
+      gateway: MyInterface
+
+    defstruct hello: "byebye"
+
+    defimpl @adaptex_gateway_api, for: @adaptex_current_adapter do
+      def foo_bar0(_adapter), do: :hello_from_adapter_foo_bar0
+      def foo_bar1(_adapter, _p1, _p2), do: :hello_from_adapter_foo_bar1
+      def foo_bar2(_adapter), do: :hello_from_adapter_foo_bar2
+    end
+  end
+
+  defmodule MyFakeGateway do
+    use Adaptex.Gateway,
+      adapter_scope: :kraken,
+      gateway: MyInterface,
+      default_adapter: MyFakeSecondaryAdapter
+  end
+
+  describe "MyFakeGateway" do
+    test "MyFakeGateway" do
+      assert MyFakeGateway.foo_bar0() == :hello_from_adapter_foo_bar0
+      assert MyFakeGateway.foo_bar1(%{coucou: :hello}, 123) == :hello_from_adapter_foo_bar1
+      assert MyFakeGateway.foo_bar2() == :hello_from_adapter_foo_bar2
+
+      Protocol.assert_protocol!(MyInterface)
+      Protocol.assert_impl!(MyInterface, MyFakeSecondaryAdapter.Adaptex)
+    end
+  end
+
+  describe "MyFakeSecondaryAdapter" do
+    test "MyFakeSecondaryAdapter" do
+      assert MyFakeSecondaryAdapter.__struct__() == %MyFakeSecondaryAdapter{hello: "byebye"}
+
+      assert Adaptex.Utils.adapter_for(MyFakeSecondaryAdapter) ==
+               MyFakeSecondaryAdapter.Adaptex
+
+      assert Adaptex.Utils.adapter_struct(MyFakeSecondaryAdapter) ==
+               %MyFakeSecondaryAdapter.Adaptex{
+                 gateway_api: AdaptexTest.MyInterface,
+                 adapter_scope: :kraken,
+                 adapter_type: "secondary"
+               }
+
+      assert MyFakeSecondaryAdapter.Adaptex.__struct__() == %MyFakeSecondaryAdapter.Adaptex{
+               gateway_api: AdaptexTest.MyInterface,
+               adapter_scope: :kraken,
+               adapter_type: "secondary"
+             }
+    end
   end
 end
